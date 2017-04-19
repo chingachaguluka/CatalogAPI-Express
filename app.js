234926db940f1e1fbbee1c5fb390641f08cc1b44
chingachaguluka <chinga.e.chaguluka@gmail.com>
2017-04-19 22:59:37 +0200

    Complete the API service

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
 
 var app = express();
 var db = mongojs('Catalog', ['products']);
 
+//Inserting body parser middleware 
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send('IT works!!');
@@ -21,6 +24,39 @@ app.get('/products', function(req, res) {
     });
 });
 
+app.post('/products', function(req, res) {
+    console.log('Inserting products...');
+    console.log(req.body);
+    db.products.insert(req.body, function(err, doc) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('sending products...');
+            res.json(doc);
+        }
+    });
+});
+
+app.put('/product/:id', function(req, res) {
+    console.log('Updating products...');
+    db.products.findAndModify({query: {_id: mongojs.ObjectId(req.params.id)},  
+                    update: {$set: {
+                        name: req.body.name,
+                        category: req.body.category,
+                        price: req.body.price
+                    }},
+                    new: true          
+                    }, function(err, doc) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('Updating products...');
+            res.json(doc);
+        }
+    });
+});
+
+
 app.get('/products/:id', function(req, res) {
     console.log('Fetching product...');
     db.products.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, doc) {
@@ -33,6 +69,18 @@ app.get('/products/:id', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
+app.delete('/products/:id', function(req, res) {
+    console.log('Fetching product...');
+    db.products.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, doc) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('Removing product...');
+            res.json(doc);
+        }
+    });
+});
+
+app.listen(5000, function() {
     console.log('Server running on port 3000...');
 });
